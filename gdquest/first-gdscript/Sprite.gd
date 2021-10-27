bf1f638c719802fda8b35fbd7c8af83741c32ef7
Kal Cantrell <klcantrell@sep.com>
2021-10-26 22:22:47 -0400

    gdquest/first-gdscript: switch if to ternary for assigning values

--- gdquest/first-gdscript/Sprite.gd
+++ gdquest/first-gdscript/Sprite.gd
@@ -4,15 +4,14 @@ const angular_speed: float = PI
 const speed: float = 400.0
 
 func _process(delta: float) -> void:
-	var direction: int = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
+	var direction: int = \
+		-1 if Input.is_action_pressed("ui_left") \
+		else 1 if Input.is_action_pressed("ui_right") \
+		else 0
 	
 	rotation += angular_speed * direction * delta
-	var velocity: Vector2 = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
+	var velocity: Vector2 = Vector2.UP.rotated(rotation) * speed \
+		if Input.is_action_pressed("ui_up") \
+		else Vector2.ZERO
 
 	position += velocity * delta
